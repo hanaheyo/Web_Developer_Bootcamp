9dff56f4a242c1fc42265aef3e75af670df3adf8
hanaheyo <violets5618@gmail.com>
2020-09-26 21:37:24 +0900

    Color Game Pt. 5

--- Color_Game/colorGame.js
+++ Color_Game/colorGame.js
@@ -4,8 +4,21 @@ var pickedColor = pickColor();
 var colorDisplay = document.querySelector("#colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
 
-colorDisplay.textContent = pickedColor;
+resetButton.addEventListener("click", function () {
+  //genertae all new colors
+  colors = generateRandomColors(6);
+  //pick a new random color from array
+  pickedColor = pickColor();
+  //change colorDisplay to match picked Color
+  colorDisplay.textContent = pickedColor;
+  //change colors of squares
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = colors[i];
+  }
+  h1.style.backgroundColor = "#232323";
+});
 
 for (var i = 0; i < squares.length; i++) {
   // add initial colors to squares
@@ -19,6 +32,7 @@ for (var i = 0; i < squares.length; i++) {
     console.log(clickedColor, pickedColor);
     if (clickedColor === pickedColor) {
       messageDisplay.textContent = "Correct!";
+      resetButton.textContent = "Play Again?";
       changeColors(clickedColor);
       h1.style.background = clickedColor;
     } else {
