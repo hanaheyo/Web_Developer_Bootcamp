c5a60ac04b38bbbdf57717f656c8580a06b76cf9
hanaheyo <violets5618@gmail.com>
2020-09-26 13:49:07 +0900

    Color Game Pt. 4

--- Color_Game/colorGame.js
+++ Color_Game/colorGame.js
@@ -1,15 +1,9 @@
-var colors = [
-  "rgb(255, 0, 0)",
-  "rgb(255, 255, 0)",
-  "rgb(0, 255, 0)",
-  "rgb(0, 255, 255)",
-  "rgb(0, 0, 255)",
-  "rgb(255, 0, 255)",
-];
+var colors = generateRandomColors(6);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.querySelector("#colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -22,9 +16,11 @@ for (var i = 0; i < squares.length; i++) {
     //grab color of clicked square
     var clickedColor = this.style.backgroundColor;
     //compare color to pickedColor
+    console.log(clickedColor, pickedColor);
     if (clickedColor === pickedColor) {
       messageDisplay.textContent = "Correct!";
       changeColors(clickedColor);
+      h1.style.background = clickedColor;
     } else {
       this.style.backgroundColor = "#232323";
       messageDisplay.textContent = "Try Again";
@@ -43,4 +39,26 @@ function changeColors(color) {
 function pickColor() {
   var random = Math.floor(Math.random() * colors.length);
   return colors[random];
+}
+
+function generateRandomColors(num) {
+  //make an array
+  var arr = [];
+  //repeat for num times
+  for (var i = 0; i < num; i++) {
+    //get random color and push into arr
+    arr.push(randomColor());
+  }
+  //return that array
+  return arr;
+}
+
+function randomColor() {
+  //pick a "red" from 0 -255
+  var red = Math.floor(Math.random() * 256);
+  //pick a "red" from 0 -255
+  var green = Math.floor(Math.random() * 256);
+  //pick a "red" from 0 -255
+  var blue = Math.floor(Math.random() * 256);
+  return "rgb(" + red + ", " + green + ", " + blue + ")";
 }
