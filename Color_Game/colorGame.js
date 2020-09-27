8cd55cba3bfc036b044bf84bc10774b637f84312
hanaheyo <violets5618@gmail.com>
2020-09-27 22:30:54 +0900

    Color Game Pt 7 (not completed)

--- Color_Game/colorGame.js
+++ Color_Game/colorGame.js
@@ -50,7 +50,7 @@ resetButton.addEventListener("click", function () {
   for (var i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = colors[i];
   }
-  h1.style.backgroundColor = "#232323";
+  h1.style.backgroundColor = "steelblue";
 });
 
 for (var i = 0; i < squares.length; i++) {
@@ -69,7 +69,7 @@ for (var i = 0; i < squares.length; i++) {
       changeColors(clickedColor);
       h1.style.background = clickedColor;
     } else {
-      this.style.backgroundColor = "#232323";
+      this.style.backgroundColor = "steelblue";
       messageDisplay.textContent = "Try Again";
     }
   });
